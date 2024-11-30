b0ff510cbdc301dcc8c7401b640bf042152e088d
BotaniheskiySad <Denisov2956151998@yandex.ru>
2024-11-30 19:25:42 +0300

    Update main.c
    добавление ошибок

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #include "func.h"
 
 int main(int argc, char** argv){
+    if (argc == 1){
+        printf("No arguments found\n");
+        return 0;
+    }
     int size; 
     for (int i = 1; i < argc; i++){
         if ((argv[i][0] == '-') && (!strchr("0123456789", argv[i][1]))){
@@ -22,10 +26,25 @@ int main(int argc, char** argv){
     }
     int i = 1;
     for (int j = 0; j < size; j++){
+        if (strspn(argv[i], "-0123456789") != strlen(argv[i])){
+            printf("Error in first argument of expression %d\n", j + 1);
+            free(results);
+            return 0;
+        }
         int arg0 = atoi(argv[i]);
         i++;
+        if (strlen(argv[i]) != 1){
+            printf("Error with operator in expression %d\n", j + 1);
+            free(results);
+            return 0;
+        }
         char op = argv[i][0];
         i++;
+        if (strspn(argv[i], "-0123456789") != strlen(argv[i])){
+            printf("Error in second argument of expression %d\n", j + 1);
+            free(results);
+            return 0;
+        }
         int arg1 = atoi(argv[i]);
         results[j] = get_ans(arg0, arg1, op);
         if (results[j] == INT_MIN){
@@ -36,8 +55,23 @@ int main(int argc, char** argv){
         i++;
     }
     print_ans(results, size);
+    if (i != argc){
+        if (strcmp(argv[i], "-k") != 0){
+            printf("ERROR: unknown flag\n");
+            free(results);
+            return 0;
+        }
         i++;
+        if (i == argc || (strspn(argv[i], "-0123456789") != strlen(argv[i]))){
+            printf("ERROR: expected integer after '-k'\n");
+            free(results);
+            return 0;
+        }
         print_ans_str(results, size, atoi(argv[i]));
+    }else{
+        printf("No flags found, end of programm\n");
     }
     free(results);
     return 0;
+}
+
